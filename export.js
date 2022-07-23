1833d292263228bdce0d4defa896c2ee6589c6d7
ahulrathore2110 <rahulg27727@gmail.com>
2022-07-23 18:46:17 +0530

    day 4 page completion

---
+++ export.js
@@ -0,0 +1,350 @@
+let brandsstrip = ()=>{
+    return ` <section id="tags">
+    <img src="https://cdn5.engagebay.com/new/assets/img/p-logos/neilpatel.png">
+    <img src="https://cdn5.engagebay.com/new/assets/img/p-logos/forbs.png">
+    <img src="https://cdn5.engagebay.com/new/assets/img/p-logos/inc.png">
+    <img src="https://cdn5.engagebay.com/new/assets/img/p-logos/enterpreneur.png">
+    <img src="https://cdn5.engagebay.com/new/assets/img/p-logos/techcrunch.png">
+    <img src="https://cdn5.engagebay.com/new/assets/img/p-logos/jeffbullas.png">
+    <img src="https://cdn5.engagebay.com/new/assets/img/p-logos/business.png">
+</section>`
+}
+
+
+let email2 = ()=>{
+    return `  <div id="getemail2">
+    <p>Simplify Marketing, Sales, & Support with the best free</p>
+    <p>All-In-One CRM software</p>
+    <div id="email2">
+        <input type="email" placeholder="Your email addres">
+        <input type="submit" value="Get Started">
+    </div>
+    <div id="check_list2">
+        <div>
+            <i class="fa-solid fa-circle-check"></i>
+            <p>Fre for 15 users</p>
+        </div>
+        <div>
+            <i class="fa-solid fa-circle-check"></i>
+            <p>No credit card requird</p>
+        </div>
+    </div>
+ 
+</div>`
+}
+
+let review_tags = ()=>{
+    return `   <div id="review">
+    <p>HAPPY CUSTOMERS SHARE GREAT REVIEWS ON G2</p>
+    <div data-aos="fade-right">
+        <img src="https://cdn5.engagebay.com/new/assets/img/Awards-2022/CRM_HighPerformer_AsiaPacific_HighPerformer.svg"
+            alt="">
+        <img src="https://cdn5.engagebay.com/new/assets/img/Awards-2022/CRM_HighPerformer_Europe_HighPerformer.svg"
+            alt="">
+        <img src="https://cdn5.engagebay.com/new/assets/img/Awards-2022/CRM_HighPerformer_HighPerformer.svg" alt="">
+        <img src="https://cdn5.engagebay.com/new/assets/img/Awards-2022/CRM_HighPerformer_Small-Business_HighPerformer.svg"
+            alt="">
+        <img src="https://cdn5.engagebay.com/new/assets/img/Awards-2022/CRM_MomentumLeader_Leader.svg" alt="">
+        <img src="https://cdn5.engagebay.com/new/assets/img/Awards-2022/EmailTracking_HighPerformer_HighPerformer.svg"
+            alt="">
+        <img src="https://cdn5.engagebay.com/new/assets/img/Awards-2022/EmailTracking_HighPerformer_Mid-Market_HighPerformer.svg"
+            alt="">
+        <img src="https://cdn5.engagebay.com/new/assets/img/Awards-2022/EmailTracking_HighPerformer_Small-Business_HighPerformer.svg"
+            alt="">
+        <img src="https://cdn5.engagebay.com/new/assets/img/Awards-2022/HelpDesk_EasiestAdmin_EaseOfAdmin.svg"
+            alt="">
+        <img src="https://cdn5.engagebay.com/new/assets/img/Awards-2022/OnlineFormBuilder_FastestImplementation_Small-Business_GoLiveTime.svg"
+            alt="">
+    </div>
+</div>`
+}
+
+
+
+let logoslider = ()=>{
+    return ` <section id="brand_slider">
+    <h1>Our Integration Partners</h1>
+    <p>Stay connected to your favorite apps using EngageBay’s integrations</p>
+    <div id="mainslider">
+        <div class="logoslide">
+            <div class="one">
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/justcall.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/twiloo.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/linkedin.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/jot.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/voxvalley.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/bulksms.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/africaltalking.png">
+                </div>
+            </div>
+            <div class="two">
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/digioh.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/piesync.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/call-hippo.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/zapier.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/sendgrid.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/mailgun.png">
+                </div>
+                
+            </div>
+            <div class="three">
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/mandrill.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/xero.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/shopify.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/quick-books.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/amazon.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/stripe.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/docusign.png">
+                </div>
+                
+            </div>
+        </div>
+
+
+
+        <div class="logoslide">
+            <div class="one">
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/justcall.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/twiloo.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/linkedin.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/jot.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/voxvalley.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/bulksms.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/africaltalking.png">
+                </div>
+            </div>
+            <div class="two">
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/digioh.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/piesync.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/call-hippo.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/zapier.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/sendgrid.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/mailgun.png">
+                </div>
+                
+            </div>
+            <div class="three">
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/mandrill.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/xero.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/shopify.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/quick-books.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/amazon.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/stripe.png">
+                </div>
+                <div>
+                    <img src="https://cdn5.engagebay.com/new/assets/img/integrations/docusign.png">
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    </section>`
+}
+
+let services = ()=>{
+    return `<div id="service">
+    <h1 data-aos="fade-up">Do It All With EngageBay</h1>
+    <p data-aos="fade-up">Stop paying thousands of dollars to dozens of marketing, sales, and support software. Experience the world's
+        most affordable, unified software. EngageBay's all-in-one CRM software is the most comprehensive solution
+        for your business.</p>
+    <div id="service_card">
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/automation.svg">
+            <p>Automation</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/calling.svg" alt="">
+            <p>Calling</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/company-management.svg" alt="">
+            <p>Comapany Management</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/contact-management.svg" alt="">
+            <p>Conatact Management</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/deal-management.svg" alt="">
+            <p>Deal management</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/email-broadcast.svg" alt="">
+            <p>Email Brodcast</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/email-sequences.svg" alt="">
+            <p>Email Sequences</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/email-template-builder.svg"
+                alt="">
+            <p>Email Template Builder</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/forms.svg" alt="">
+            <p>Forms</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/help-desk.svg" alt="">
+            <p>Help Desk</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/landing-pages.svg" alt="">
+            <p>Landing pages</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/live-chat.svg" alt="">
+            <p>Live chat</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/milestone-tracking.svg" alt="">
+            <p>Milestone Tracking</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/calendar.svg" alt="">
+            <p>Calender </p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/products.svg" alt="">
+            <p>Products</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/proposal.svg" alt="">
+            <p>Proposal</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/push-notifications.svg" alt="">
+            <p>Push Notification</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/segmentation.svg" alt="">
+            <p>Segmantation</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/site-messaging.svg" alt="">
+            <p>Site management</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/sms-broadcast.svg" alt="">
+            <p>SMS Brodcast</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/social-posts-scheduling.svg"
+                alt="">
+            <p>Soical Post Scheduling</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/social-suite.svg" alt="">
+            <p>Social Suite</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/sticky-bars.svg" alt="">
+            <p>Sticky bars</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/task-management.svg" alt="">
+            <p>Task Management</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/team-management.svg" alt="">
+            <p>Team management</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/video-templates.svg" alt="">
+            <p>Video Templates</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/web-analytics.svg" alt="">
+            <p>Web analaytics</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/ab-landing-pages.svg" alt="">
+            <p>A/B Landing Pages</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/custom-reporting.svg" alt="">
+            <p>Custom Reporting</p>
+        </div>
+        <div data-aos="fade-up">
+            <img src="https://cdn5.engagebay.com/new/assets/img/icons/eb-features/workflows.svg" alt="">
+            <p>Workflows</p>
+        </div>
+
+    </div>
+</div>`
+}
+
+export {brandsstrip , email2 , review_tags , logoslider,services};
